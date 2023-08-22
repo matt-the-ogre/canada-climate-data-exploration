907319d53a3d08303fd5acab5dd8f9dd533cc7e0
matt-manuel <68288703+matt-manuel@users.noreply.github.com>
2023-08-22 14:28:55 -0700

    import stations does data cleaning

---
+++ get-hourly-data-by-station.py
@@ -0,0 +1,88 @@
+import sqlite3
+import logging
+import argparse
+import os
+import requests
+
+def get_hourly_data_years(database_path, station_id):
+    # Connect to the SQLite database
+    conn = sqlite3.connect(database_path)
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    # SQL query to retrieve "HLY First Year" and "HLY Last Year" for the given station ID
+    query = """
+    SELECT "Name", "Province", "HLY First Year", "HLY Last Year"
+    FROM stations
+    WHERE "Station ID" = ? AND "HLY First Year" IS NOT NULL
+    """
+
+    # Execute the query with the provided station ID
+    cursor.execute(query, (station_id,))
+
+    # Fetch the result
+    result = cursor.fetchone()
+    logging.info(f"Result: {result}")
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+    # If a result was found, return it as a tuple; otherwise, return (0, 0)
+    if result:
+        return result
+    else:
+        return (0, 0)
+
+def download_station_data(station_id, year, month, timeframe):
+    # Define the URL with the given parameters
+    url = f"https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID={station_id}&Year={year}&Month={month}&Day=14&timeframe={timeframe}&submit=Download+Data"
+
+    # Define the directory path
+    directory_path = os.path.join("data", str(station_id), str(year))
+
+    # Ensure the directory exists
+    os.makedirs(directory_path, exist_ok=True)
+
+    # Make a GET request to the URL
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # Get the filename from the content disposition header
+    content_disposition = response.headers.get("content-disposition")
+    filename = content_disposition.split("filename=")[-1].strip("\"")
+
+    # Define the file path
+    file_path = os.path.join(directory_path, filename)
+
+    # Write the content to the file
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+    logging.info(f"Downloaded file for station {station_id}, year {year}, month {month} to '{file_path}'.")
+
+
+def main(args):
+    name, province, first_year, last_year = get_hourly_data_years(args.database_path, args.station_id)
+    logging.info(f"Hourly data available for station {args.station_id} ({name}, {province}): {first_year} to {last_year}")
+    
+    # Example usage
+    station_id = args.station_id
+    year = first_year
+    month = 1
+    timeframe = 1
+
+    download_station_data(station_id, year, month, timeframe)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Retrieve hourly data years for a given station ID.")
+    parser.add_argument('station_id', type=int, help="Station ID to look up")
+    parser.add_argument('--database_path', type=str, default='database.db', help="Path to the SQLite database")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    args = parse_arguments()
+    main(args)
--- import-stations.py
+++ import-stations.py
@@ -1,6 +1,39 @@
 import sqlite3
 import pandas as pd
 import logging
+import shutil
+import requests
+import os
+
+# Function to download the stations file
+def download_stations_file(url, directory='data'):
+    # Ensure the directory exists
+    os.makedirs(directory, exist_ok=True)
+
+    # Download the file from the URL
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # Paths for the downloaded file, backup file, and final file
+    download_path = os.path.join(directory, 'stations.csv')
+    backup_path = os.path.join(directory, 'stations.csv.bak')
+
+    # Save the downloaded file
+    with open(download_path, 'wb') as file:
+        file.write(response.content)
+
+    # Backup the downloaded file
+    shutil.copy(download_path, backup_path)
+
+    # Read the file, skipping the first three lines
+    # Note: We skip the first three lines because they have header / metadata information
+    # and the CSV import won't work correctly without the column names being at the first line of the file
+    df = pd.read_csv(download_path, skiprows=3)
+
+    # Save the modified file back to 'stations.csv'
+    df.to_csv(download_path, index=False)
+
+    logging.info(f"Downloaded and processed 'stations.csv' in the '{directory}' directory.")
 
 # Function to get the count of stations from the database
 def get_station_count(conn):
@@ -14,94 +47,83 @@ def get_station_count(conn):
         # Return 0 if the "stations" table doesn't exist
         return 0
 
-def delete_stations_without_location(conn):
-    try:
-        cursor = conn.cursor()
-        # count the stations that will be deleted before deleting them
-        cursor.execute("SELECT COUNT(*) FROM stations WHERE Latitude = -99999 AND Longitude = -99999")
-        count = cursor.fetchone()[0]
-        logging.info(f"Number of stations without location data: {count}")
-        # delete the stations that have missing location data
-        cursor.execute("DELETE FROM stations WHERE Latitude = -99999 AND Longitude = -99999")
-        cursor.close()
-    except sqlite3.OperationalError:
-        pass
-
 def convert_zero_to_null(conn):
     try:
         cursor = conn.cursor()
-        # count the stations that will be deleted before deleting them
-        # cursor.execute("SELECT COUNT(*) FROM stations WHERE \"HLY First Year\" = 0")
-        # logging.info(f"Number of stations with \"HLY First Year\" = 0: {cursor.fetchone()[0]}")
         cursor.execute("UPDATE stations SET \"HLY First Year\" = NULL WHERE \"HLY First Year\" = 0")
         cursor.execute("UPDATE stations SET \"HLY Last Year\" = NULL WHERE \"HLY Last Year\" = 0")
         cursor.execute("UPDATE stations SET \"DLY First Year\" = NULL WHERE \"DLY First Year\" = 0")
         cursor.execute("UPDATE stations SET \"DLY Last Year\" = NULL WHERE \"DLY Last Year\" = 0")
         cursor.execute("UPDATE stations SET \"MLY First Year\" = NULL WHERE \"MLY First Year\" = 0")
         cursor.execute("UPDATE stations SET \"MLY Last Year\" = NULL WHERE \"MLY Last Year\" = 0")
-        # cursor.execute("SELECT COUNT(*) FROM stations WHERE \"HLY First Year\" IS NULL")
-        # logging.info(f"Number of stations with \"HLY First Year\" = NULL: {cursor.fetchone()[0]}")
         conn.commit()
         cursor.close()
     except sqlite3.OperationalError:
         pass
 
-logging.basicConfig(level=logging.INFO)
 
-# Read the CSV file into a Pandas DataFrame
-# Note that some rows have missing values for the "Latitude" and "Longitude" columns
-# I'm going to delete those rows after importing the CSV data into the database
-# because I want to map all the stations later
-csv_file = 'data/stations.csv'
-df = pd.read_csv(csv_file)
+def main(args):
+    logging.basicConfig(level=logging.INFO)
+    # Read the CSV file into a Pandas DataFrame
+    # Note that some rows have missing values for the "Latitude" and "Longitude" columns
+    # I'm going to delete those rows after importing the CSV data into the dataframe
+    # because I want to map all the stations later
+    
+    # Call the function to download and process the file
+    # URL of the CSV file
+    url = 'https://collaboration.cmc.ec.gc.ca/cmc/climate/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv'
+    download_stations_file(url)
 
-# fill NA values with -99999 for the "Latitude" and "Longitude" columns
-df['Latitude'].fillna(-99999, inplace=True)
-df['Longitude'].fillna(-99999, inplace=True)
+    csv_file = 'data/stations.csv'
+    df = pd.read_csv(csv_file)
 
-# convert the "Latitude" and "Longitude" columns to integers
-df['Latitude'] = df['Latitude'].astype(int)
-df['Longitude'] = df['Longitude'].astype(int)
+    # delete the rows that have missing values for the "Latitude" and "Longitude" columns
+    df = df.dropna(subset=['Latitude', 'Longitude'])
 
-# fill NA values with 0 for the "HLY First Year", "HLY Last Year", "DLY First Year", "DLY Last Year", "MLY First Year", and "MLY Last Year" columns
-df['HLY First Year'].fillna(0, inplace=True)
-df['HLY Last Year'].fillna(0, inplace=True)
-df['DLY First Year'].fillna(0, inplace=True)
-df['DLY Last Year'].fillna(0, inplace=True)
-df['MLY First Year'].fillna(0, inplace=True)
-df['MLY Last Year'].fillna(0, inplace=True)
+    # convert the "Latitude" and "Longitude" columns to integers
+    df['Latitude'] = df['Latitude'].astype(int)
+    df['Longitude'] = df['Longitude'].astype(int)
 
-# convert the "HLY First Year", "HLY Last Year", "DLY First Year", "DLY Last Year", "MLY First Year", and "MLY Last Year" columns to integers
-df['HLY First Year'] = df['HLY First Year'].astype(int)
-df['HLY Last Year'] = df['HLY Last Year'].astype(int)
-df['DLY First Year'] = df['DLY First Year'].astype(int)
-df['DLY Last Year'] = df['DLY Last Year'].astype(int)
-df['MLY First Year'] = df['MLY First Year'].astype(int)
-df['MLY Last Year'] = df['MLY Last Year'].astype(int)
+    # fill NA values with 0 for the "HLY First Year", "HLY Last Year", "DLY First Year", "DLY Last Year", "MLY First Year", and "MLY Last Year" columns
+    df['HLY First Year'].fillna(0, inplace=True)
+    df['HLY Last Year'].fillna(0, inplace=True)
+    df['DLY First Year'].fillna(0, inplace=True)
+    df['DLY Last Year'].fillna(0, inplace=True)
+    df['MLY First Year'].fillna(0, inplace=True)
+    df['MLY Last Year'].fillna(0, inplace=True)
 
-# Connect to the SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('database.db')
+    # convert the "HLY First Year", "HLY Last Year", "DLY First Year", "DLY Last Year", "MLY First Year", and "MLY Last Year" columns to integers
+    df['HLY First Year'] = df['HLY First Year'].astype(int)
+    df['HLY Last Year'] = df['HLY Last Year'].astype(int)
+    df['DLY First Year'] = df['DLY First Year'].astype(int)
+    df['DLY Last Year'] = df['DLY Last Year'].astype(int)
+    df['MLY First Year'] = df['MLY First Year'].astype(int)
+    df['MLY Last Year'] = df['MLY Last Year'].astype(int)
 
-# Get the number of stations before the import
-before_count = get_station_count(conn)
+    # Connect to the SQLite database (or create it if it doesn't exist)
+    conn = sqlite3.connect('database.db')
 
-# Create a table and import the CSV data into it
-df.to_sql('stations', conn, if_exists='replace', index=False)
+    # Get the number of stations before the import
+    before_count = get_station_count(conn)
 
-# Get the number of stations after the import
-after_count = get_station_count(conn)
+    # Create a table and import the CSV data into it
+    df.to_sql('stations', conn, if_exists='replace', index=False)
 
-# Print the results
-logging.info(f"Number of stations before the import: {before_count}")
-logging.info(f"Number of new stations added: {after_count - before_count}")
+    # Get the number of stations after the import
+    after_count = get_station_count(conn)
 
-# Delete the stations that have missing location data
-delete_stations_without_location(conn)
+    # Print the results
+    logging.info(f"Number of stations before the import: {before_count}")
+    logging.info(f"Number of new stations added: {after_count - before_count}")
 
-logging.info(f"Number of stations after deleting stations without location data: {get_station_count(conn)}")
+    # Convert 0 values to NULL
+    convert_zero_to_null(conn)
 
-# Convert 0 values to NULL
-convert_zero_to_null(conn)
+    # Close the connection
+    conn.close()
 
-# Close the connection
-conn.close()
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Import stations into the database.')
+    args = parser.parse_args()
+    main(args)
